e5fb4988f95c7ab6a282c434bd9b2137860d7780
hermsale <hermsale@gmail.com>
2022-11-25 16:11:17 -0300

    se agrego estilos con una hoja externa

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './TodoCounter.css';
+
 
 function TodoCounter () {
     return(
-        <h2> Has completado 2 de 3 TODO's</h2>
+        <h2 className='TodoCounter'> Has completado 2 de 3 TODO's</h2>
     );
 }
 
