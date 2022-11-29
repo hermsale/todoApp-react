def3a6afd5e04449a026acfa6ae9a6f58d2474a3
hermsale <hermsale@gmail.com>
2022-11-29 17:33:27 -0300

    se empezo a trabajar en la logica de eventos y funciones

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -3,7 +3,7 @@ import './CreateTodoButton.css' ;
 
 function CreateTodoButton() {
 return (
-    <button className='CreateTodoButton'>+</button>
+    <button className='CreateTodoButton'  onClick={console.log('click')}>+</button>
 )
 };
 
