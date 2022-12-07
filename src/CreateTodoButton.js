b212009b0232edeb7533a11f8896d62168d994e7
Alejandro Hugo Herms <hermsale@gmail.com>
2022-12-07 16:07:20 -0300

    se trabajo en los eventos de CreateTodoButton y TodoItem

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './CreateTodoButton.css' ;
 
+
+
 function CreateTodoButton() {
-return (
-    <button className='CreateTodoButton'  onClick={() => console.log('click')}>+</button>
-)
-};
+    // funcion para crear un Todo / recibe un argumento con texto 
+    const onClickButton = (msg) => {
+        alert(msg);
+    };
+
+    // enviamos los parametros para la funcion 
+    return (
+        <button className='CreateTodoButton'  onClick = { () => onClickButton('Esto crea un TODO')}>+</button>
+        )
+    };
 
 export {CreateTodoButton};
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,12 +3,24 @@ import './TodoItem.css'
 import { FaCheck, FaHotjar } from "react-icons/fa";
 
 function TodoItem(props){
-    // console.log(props);
+    
+    const onComplete = () =>{
+        alert ('Completaste el TODO '+props.text);
+    }
+
+    const onDelete = () =>{
+        alert ('Borraste el TODO '+props.text);
+    }
+    
     return (
         <li className='TodoItem'>      
-            <span className={`Icon Icon__check ${props.completed && 'Icon__check-active'}`}><FaCheck /></span>
+            <span className={`Icon Icon__check ${props.completed && 'Icon__check-active'}`}
+                onClick={onComplete}
+            ><FaCheck /></span>
             <p className={`TodoItem__p ${props.completed && 'TodoItem__p-completed'}`}>{props.text}</p>
-            <span className='Icon Icon__delete'><FaHotjar/></span>
+            <span className='Icon Icon__delete'
+                onClick={onDelete}
+            ><FaHotjar/></span>
         </li>
     );
 }
