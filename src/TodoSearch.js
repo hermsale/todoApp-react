d612a6e7f3c68578eeb6172ad17e6252e410415e
hermsale <hermsale@gmail.com>
2022-12-12 16:41:29 -0300

    se definio el uso de onChange en el TodoSearch

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,9 +2,22 @@ import React from "react";
 import './TodoSearch.css'
 
 function TodoSearch (){
+    // creamos un const para guardar el estado
+    const estado = React.useState();
+
+    const onSearchValueChange = (event) =>{
+       console.log(event.target.value);
+    }
+
     return (
-        <input className="TodoSearch" placeholder='Cebolla'/>
-    );
+        <input 
+            className="TodoSearch"
+            placeholder='Cebolla'
+            onChange={onSearchValueChange}
+        />
+     
+    )
+
 }
 
 export {TodoSearch};
