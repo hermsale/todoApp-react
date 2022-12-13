d1ce079e4bacad9637b4528c4452c8531793f875
hermsale <hermsale@gmail.com>
2022-12-13 15:29:27 -0300

    cambios minimos en searchValue

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,21 +2,27 @@ import React from "react";
 import './TodoSearch.css'
 
 function TodoSearch (){
-    // creamos un const para guardar el estado
-    const estado = React.useState();
+    // creamos un array que guarde un estado y una funcion, propio del objeto React.useState
+    const [searchValue, setSearchValue] = React.useState('');
 
+    // cada vez que hagamos un cambio en el input, va a llamar a la funcion setSearchValue para actualizar su estado
     const onSearchValueChange = (event) =>{
        console.log(event.target.value);
+    //    por medio de la funcion, setteamos un valor nuevo al estado searchValue
+       setSearchValue(event.target.value);
     }
 
-    return (
+    return [
         <input 
             className="TodoSearch"
             placeholder='Cebolla'
+            // indicamos que el valor sera igual al del estado
+            value={searchValue}
+            // cada cambio que haya en el input, se transladará a la funcion
             onChange={onSearchValueChange}
-        />
-     
-    )
+        />,
+        <p>{searchValue}</p>
+    ]
 
 }
 
