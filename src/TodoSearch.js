10214f3233e4b0b69b946953ec6839b1a4dab8d2
Alejandro Hugo Herms <hermsale@gmail.com>
2022-12-14 13:27:32 -0300

    se modifico el TodoSearch

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,10 +1,26 @@
 import React from "react";
-import './TodoSearch.css'
+import "./TodoSearch.css";
 
-function TodoSearch (){
-    return (
-        <input className="TodoSearch" placeholder='Cebolla'/>
-    );
+function TodoSearch() {
+  // react Hooks
+  const [searchValue, setSearchValue] = React.useState('');
+
+  // tomamos por parametro el objeto event de los cambios del input 
+  const onSearchValueChange = (event) => {
+    console.log(event.target.value);
+    setSearchValue(event.target.value);
+  };
+
+  return [
+    <input
+      className="TodoSearch"
+      placeholder="Cebolla"
+      value={searchValue}
+      // tomamos todos los cambios en el input
+      onChange={onSearchValueChange}
+    />,
+    <p>{searchValue}</p>
+  ];
 }
 
-export {TodoSearch};
+export { TodoSearch };
