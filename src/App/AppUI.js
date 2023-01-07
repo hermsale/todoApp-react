da4d0881324713033afdb022adf13c3069f8e894
Alejandro Hugo Herms <hermsale@gmail.com>
2023-01-07 15:20:52 -0300

    se agrego un AppUI para el manejo de la interfaz, respetando la arquitectura de componentes stateful y stateless

---
+++ src/App/AppUI.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { CreateTodoButton } from '../CreateTodoButton';
+
+function AppUI({ totalTodos, completedTodos, searchValue, setSearchValue, searchedTodos, toggleCompleteTodo, deleteTodo }) {
+    return (
+        <React.Fragment>
+            {/* le pasamos al componente encargado de tomar el control de Todo's los valores */}
+            <TodoCounter
+                total={totalTodos}
+                completed={completedTodos}
+            />
+
+            {/* pasamos por props el estado  */}
+            <TodoSearch
+                searchValue={searchValue}
+                setSearchValue={setSearchValue}
+            />
+            {/* podemos hacer uso en TodoList de la propiedad children, ya que  hicimos apertura y cierre del componente */}
+            <TodoList>
+                {searchedTodos.map((todo) => (
+                    <TodoItem
+                        key={todo.index}
+                        number={todo.index}
+                        text={todo.text}
+                        completed={todo.completed}
+                        onComplete={() => toggleCompleteTodo(todo.index)}
+                        onDelete={() => deleteTodo(todo.index)}
+                    />
+                ))}
+            </TodoList>
+            <CreateTodoButton />
+        </React.Fragment>
+    );
+}
+
+export { AppUI };
--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import { TodoCounter } from '../TodoCounter';
-import { TodoSearch } from '../TodoSearch';
-import { TodoList } from '../TodoList';
-import { TodoItem } from '../TodoItem';
-import { CreateTodoButton } from '../CreateTodoButton';
+import { AppUI } from './AppUI';
 
 
 // creamos una lista de array con objetos y propiedades - tareas pendientes
@@ -82,27 +78,15 @@ function App() {
 
 
   return (
-    <React.Fragment>
-      {/* le pasamos al componente encargado de tomar el control de Todo's los valores */}
-      <TodoCounter total={totalTodos} completed={completedTodos} />
-
-      {/* pasamos por props el estado  */}
-      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
-      {/* podemos hacer uso en TodoList de la propiedad children, ya que  hicimos apertura y cierre del componente */}
-      <TodoList>
-        {searchedTodos.map((todo) => (
-          <TodoItem
-            key={todo.index}
-            number={todo.index}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete = {() => toggleCompleteTodo(todo.index)}
-            onDelete = {() => deleteTodo(todo.index)}
-          />
-        ))}
-      </TodoList>
-      <CreateTodoButton />
-    </React.Fragment>
+    <AppUI 
+        total={totalTodos} 
+        completed={completedTodos} 
+        searchValue={searchValue} 
+        setSearchValue={setSearchValue} 
+        searchedTodos={searchedTodos}
+        toggleCompleteTodo={toggleCompleteTodo}
+        deleteTodo={deleteTodo}
+    />
   );
 }
 
